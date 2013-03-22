bb7d8143f88eba84186310380ce51c7847faaefe
ariandyy <ariandyy@gmail.com>
2013-03-22 16:02:00 +0100

    meh

--- src/colorblobtracking.cpp
+++ src/colorblobtracking.cpp
@@ -1,16 +1,13 @@
 #include "ros/ros.h"
-
-//#include "opencv2/opencv.hpp"
-//#include "opencv2/highgui/highgui.hpp"
 #include "opencv/cv.h"
 #include "opencv/highgui.h"
 
 //TODO:
-// - implement color blob centroid calculation, ref: http://home.wlu.edu/~levys/software/ardrone_autopylot/greenball_agent.py
+// - make centroid calculation more robust
 // - implement publishing messages (x and y position of the blob centroid), ref: ROS beginner tutorials
 
-IplImage* GetThresholdedImg(IplImage* img, int lo_h, int lo_s, int lo_v, int hi_h, int hi_s, int hi_v);
-CvPoint CalculateCentroid(IplImage* threshedImg);
+IplImage* getHSVThresholdedImg(IplImage* img, int lo_h, int lo_s, int lo_v, int hi_h, int hi_s, int hi_v);
+CvPoint calculateCentroid(IplImage* threshedImg);
 
 using namespace cv;
 
@@ -26,19 +23,19 @@ int main(int argc, char** argv)
 	}
 	
 	
-	cvNamedWindow("webcam", CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("thresh", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("Webcam", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("Output", CV_WINDOW_AUTOSIZE);
 	
 	// HSV max and min values in opencv
 	//int lo_h = 0, lo_s = 0, lo_v = 0, hi_h = 180, hi_s = 255, hi_v = 255;
 	// this is for green
-	int lo_h = 34, lo_s = 36, lo_v = 26, hi_h = 43, hi_s = 164, hi_v = 213;
-	createTrackbar("Lo H", "thresh", &lo_h, 255);
-	createTrackbar("Lo S", "thresh", &lo_s, 255);
-	createTrackbar("Lo V", "thresh", &lo_v, 255);
-	createTrackbar("Hi H", "thresh", &hi_h, 180);
-	createTrackbar("Hi S", "thresh", &hi_s, 255);
-	createTrackbar("Hi V", "thresh", &hi_v, 255);
+	int lo_h = 23, lo_s = 68, lo_v = 81, hi_h = 60, hi_s = 174, hi_v = 228;
+	createTrackbar("Lo H", "Output", &lo_h, 255);
+	createTrackbar("Lo S", "Output", &lo_s, 255);
+	createTrackbar("Lo V", "Output", &lo_v, 255);
+	createTrackbar("Hi H", "Output", &hi_h, 180);
+	createTrackbar("Hi S", "Output", &hi_s, 255);
+	createTrackbar("Hi V", "Output", &hi_v, 255);
 	
 	while(1){
 		IplImage* srcImg = cvQueryFrame(capture);
@@ -47,46 +44,44 @@ int main(int argc, char** argv)
 			return 1;			
 		}
 
-		IplImage* threshImg = GetThresholdedImg(srcImg, lo_h, lo_s, lo_v, hi_h, hi_s, hi_v);
+		IplImage* threshImg = getHSVThresholdedImg(srcImg, lo_h, lo_s, lo_v, hi_h, hi_s, hi_v);
 		
 		// Centroid calculation
-		CvPoint centroid = CalculateCentroid(threshImg);
+		CvPoint centroid = calculateCentroid(threshImg);
 		
-		cvCircle(srcImg, centroid, 5, cvScalar(0, 255, 255), 5);
+		cvCircle(srcImg, centroid, 5, cvScalar(0, 0, 255), 5);
 		
-		cvShowImage("webcam", srcImg);
-		cvShowImage("thresh", threshImg);
+		cvShowImage("Webcam", srcImg);
+		cvShowImage("Output", threshImg);
 		
 		// press ESC to exit
 		if ( (cvWaitKey(10) & 255) == 27 ) break;
 	}
 	
 	cvReleaseCapture(&capture);
-	cvDestroyWindow("webcam");
-	cvDestroyWindow("thresh");
+	cvDestroyWindow("Webcam");
+	cvDestroyWindow("Output");
 	
 	return 0;
 }
 
 // Ref: http://www.aishack.in/2010/07/tracking-colored-objects-in-opencv/
-IplImage* GetThresholdedImg(IplImage* img, int lo_h, int lo_s, int lo_v, int hi_h, int hi_s, int hi_v)
+IplImage* getHSVThresholdedImg(IplImage* img, int lo_h, int lo_s, int lo_v, int hi_h, int hi_s, int hi_v)
 {
 	IplImage* imgHSV = cvCreateImage(cvGetSize(img), 8, 3);
 	cvCvtColor(img, imgHSV, CV_BGR2HSV);
 	
 	IplImage* imgThresh = cvCreateImage(cvGetSize(img), 8, 1);
-	//cvInRangeS(imgHSV, cvScalar(200, 18, 90), cvScalar(230, 60, 100), imgThresh);
 	cvInRangeS(imgHSV, cvScalar(lo_h, lo_s, lo_v), cvScalar(hi_h, hi_s, hi_v), imgThresh);
 	
 	cvErode(imgThresh, imgThresh);
-	cvDilate(imgThresh, imgThresh);
 	
 	cvReleaseImage(&imgHSV);
 	return imgThresh;
 }
 
 // Ref: http://stackoverflow.com/questions/8143414/centroid-of-contour-object-in-opencv-in-c
-CvPoint CalculateCentroid(IplImage* threshedImg)
+CvPoint calculateCentroid(IplImage* threshedImg)
 {
 	int centroid_x = 0, centroid_y = 0;
 	CvMoments m;
