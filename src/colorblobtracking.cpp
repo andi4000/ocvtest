54fa386e55625bcf586bad4dfdda069e524e0b00
ariandyy <ariandyy@gmail.com>
2013-03-26 18:04:26 +0100

    colorblob tracking works, base for ROS message publishing

--- src/colorblobtracking.cpp
+++ src/colorblobtracking.cpp
@@ -7,11 +7,16 @@
 #include <stdlib.h>
 
 //TODO:
-// - make centroid calculation more robust (hint: simple blob detector, findcontour)
-// - implement publishing messages (x and y position of the blob centroid), ref: ROS beginner tutorials
+// - make everything standardized CPP!
+
+//TODO:
+// messages:
+// - object_detected state
+// - x position
+// - y position
+// - area
 
 IplImage* getHSVThresholdedImg(IplImage* img, int lo_h, int lo_s, int lo_v, int hi_h, int hi_s, int hi_v);
-CvPoint calculateCentroid(IplImage* threshedImg);
 
 using namespace cv;
 using namespace std;
@@ -27,9 +32,15 @@ int main(int argc, char** argv)
 		ROS_INFO("Opening webcam");
 	}
 	
+	int capSizeX = 640;
+	int capSizeY = 480;
 	
 	cvNamedWindow("Webcam", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("Output", CV_WINDOW_AUTOSIZE);
+	
+	cvMoveWindow("Webcam", 0*capSizeX, 0);
+	cvMoveWindow("Output", 1*capSizeX, 0);
+	
 	
 	// HSV max and min values in opencv
 	//int lo_h = 0, lo_s = 0, lo_v = 0, hi_h = 180, hi_s = 255, hi_v = 255;
@@ -51,48 +62,6 @@ int main(int argc, char** argv)
 
 		IplImage* threshImg = getHSVThresholdedImg(srcImg, lo_h, lo_s, lo_v, hi_h, hi_s, hi_v);
 		
-		//TODO: THIS PART IS NOT RELIABLE
-		//TODO: http://www.bytefish.de/blog/extracting_contours_with_opencv
-		/**
-		// contour test begin
-		// Page 243-244
-		// http://stackoverflow.com/questions/6044119/opencv-cvfindcontours-how-do-i-separate-components-of-a-contour
-		// http://books.google.de/books?id=seAgiOfu2EIC&pg=PA234&lpg=PA234&dq=cvFindContours&source=bl&ots=hSJ3eleCKa&sig=tdP8PSgr1azn2Im8rmqSGhzonBU&hl=en&sa=X&ei=QW1MUc3TCIXltQaB94CwAQ&ved=0CGUQ6AEwBQ#v=onepage&q=cvFindContours&f=false
-		CvMemStorage* storage = cvCreateMemStorage();
-		CvSeq* first_contour = NULL;
-		CvSeq* first_polygon = NULL;
-		
-		//int Nc = cvFindContours(threshImg, storage, &first_contour, sizeof(CvContour), CV_RETR_LIST);
-		int Nc = cvFindContours(threshImg, storage, &first_contour, sizeof(CvContour), CV_RETR_EXTERNAL);
-		
-		first_polygon = cvApproxPoly(first_contour, sizeof(CvContour), storage, CV_POLY_APPROX_DP, 2, 1);
-		
-		ROS_INFO("Total contours detected: %d", Nc);
-		
-		
-		CvSeq* biggest_part;
-		double area_buf = 0;
-		double biggest_area = 0;
-		// find the biggest contour area
-		for( CvSeq* c = first_contour; c != NULL; c = c->h_next) {
-			biggest_area = cvContourArea(c);
-			if (biggest_area > area_buf){
-				area_buf = biggest_area;
-				biggest_part = c;
-			}
-		}
-		ROS_INFO("Biggest area: %d", biggest_area);
-		
-		
-		for( CvSeq* c = biggest_part; c != NULL; c = c->h_next) {
-		//for( CvSeq* c = first_polygon; c != NULL; c = c->h_next) {
-			//cvDrawContours(srcImg, c, CVX_RED, CVX_BLUE, 0, 2, 8);
-			cvDrawContours(srcImg, c, CV_RGB(255, 0, 0), CV_RGB(0, 0, 0), -1, CV_FILLED, 8);
-		}
-		*/
-		// contour test end
-		
-		
 		// CONTOUR TEST PART2 BEGIN
 		// source: http://www.bytefish.de/blog/extracting_contours_with_opencv
 		Mat threshMat(threshImg);
@@ -108,24 +77,53 @@ int main(int argc, char** argv)
 		double max_area;
 		Point maxPosition;
 		minMaxLoc(Mat(areas), 0, &max_area, 0, &maxPosition);
-		ROS_INFO("biggest area = %f", max_area);
+		//ROS_INFO("biggest area = %f", max_area);
 		
+		bool got_it = false;
+		if (max_area > 2000) got_it = true;
+		
+		if (got_it)
+			ROS_INFO("got it");
+		else
+			ROS_INFO("nothing");
 		// draw only the biggest contour
+		
 		drawContours(srcMat, contours, maxPosition.y, Scalar(0, 0, 255), CV_FILLED);
 		
 		//TODO:
-		// - find the centroid
-		// - output the centroid x y and area
+		// - put each on functions!
 		
-		imshow("Webcam", srcMat);
+		// GETTING THE CENTROID BEGIN
+		Point image_centroid(0, 0);
+		Point relative_centroid(0, 0);
+		if (got_it){
+			vector<Moments> mu(contours.size());
+			for ( int i = 0; i < contours.size(); i++ ) {
+				mu[i] = moments(contours[i], false);
+			}
+			
+			vector<Point> mc(contours.size());
+			for ( int i = 0; i < contours.size(); i++ ) {
+				mc[i] = Point( mu[i].m10/mu[i].m00, mu[i].m01/mu[i].m00 );
+			}
+			
+			// draw the centroid of the biggest contour
+			image_centroid = mc[maxPosition.y];
+			circle(srcMat, image_centroid, 4, Scalar(255, 0, 0), -1, 8, 0);
+			//ROS_INFO("image centroid (x, y) = (%d, %d)", image_centroid.x, image_centroid.y);
+			
+			// getting the relative centroid (middle point as 0,0)
+			relative_centroid.x = image_centroid.x - (capSizeX / 2);
+			relative_centroid.y = -image_centroid.y + (capSizeY / 2);
+			ROS_INFO("relative centroid (x, y) = (%d, %d) area %.2f", relative_centroid.x, relative_centroid.y, max_area);
+		}
+		// GETTING THE CENTROID END
+		
+		
+		
 		imshow("Output", threshMat);
+		imshow("Webcam", srcMat);
 		// CONTOUR TEST PART2 END
-		
-		
-		
-		// Centroid calculation and display
-		//CvPoint centroid = calculateCentroid(threshImg);
-		//cvCircle(srcImg, centroid, 5, cvScalar(0, 0, 255), 5);
 		
 		//cvShowImage("Webcam", srcImg);
 		//cvShowImage("Output", threshImg);
@@ -154,22 +152,4 @@ IplImage* getHSVThresholdedImg(IplImage* img, int lo_h, int lo_s, int lo_v, int 
 	
 	cvReleaseImage(&imgHSV);
 	return imgThresh;
-}
-
-// Ref: http://stackoverflow.com/questions/8143414/centroid-of-contour-object-in-opencv-in-c
-CvPoint calculateCentroid(IplImage* threshedImg)
-{
-	int centroid_x = 0, centroid_y = 0;
-	CvMoments m;
-	double m00, m01, m10;
-	
-	cvMoments(threshedImg, &m, true);
-	m00 = cvGetSpatialMoment(&m, 0, 0);
-	m01 = cvGetSpatialMoment(&m, 0, 1);
-	m10 = cvGetSpatialMoment(&m, 1, 0);
-	
-	centroid_x = (int) m10 / m00;
-	centroid_y = (int) m01 / m00;
-	
-	return cvPoint(centroid_x, centroid_y);
 }
